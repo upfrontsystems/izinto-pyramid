583d861b5b3abb60d66ec4497af4f52ccce25da7
nvernooy <vernooy.n@gmail.com>
2019-11-21 12:01:56 +0200

    Fix migration user_id to dashboard

--- izinto/alembic/versions/20191119_6f25480d827e.py
+++ izinto/alembic/versions/20191119_6f25480d827e.py
@@ -49,8 +49,13 @@ def upgrade():
     conn = op.get_bind()
     # copy users from user_id to user_dashboard
     for dashboard in conn.execute("select * from dashboard"):
+        user_id = dashboard.user_id
+        if not user_id:
+            admin_role = conn.execute("select * from role where name = 'Administrator'").first()
+            admin = conn.execute("select * from user_role where role_id = %s" % admin_role.id).first()
+            user_id = admin.user_id
         conn.execute("insert into user_dashboard (user_id, dashboard_id) values ('%s', %s)" %
-                     (dashboard.user_id, dashboard.id))
+                     (user_id, dashboard.id))
 
     op.drop_constraint('fk_dashboard_user_id_user', 'dashboard', type_='foreignkey')
     op.drop_column('dashboard', 'user_id')
@@ -63,8 +68,15 @@ def upgrade():
 def downgrade():
     op.add_column('dashboard', sa.Column('user_id', sa.VARCHAR(length=32), autoincrement=False, nullable=True))
     op.create_foreign_key('fk_dashboard_user_id_user', 'dashboard', 'user', ['user_id'], ['id'], ondelete='CASCADE')
+
+    conn = op.get_bind()
+    for user_dash in conn.execute("select * from user_dashboard"):
+        conn.execute("update dashboard set user_id = '%s' where id = %s" % (user_dash.user_id, user_dash.dashboard_id))
+
     op.drop_table('user_dashboard')
     op.drop_table('user_collection')
     op.drop_table('dashboard_collection')
     op.drop_index(op.f('ix_collection_id'), table_name='collection')
+    op.drop_constraint('fk_dashboard_collection_id_collection', 'dashboard', type_='foreignkey')
+    op.drop_column('dashboard', 'collection_id')
     op.drop_table('collection')

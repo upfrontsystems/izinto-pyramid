b73412b7b0df50b52dda044df066a13bb46544c5
nvernooy <vernooy.n@gmail.com>
2021-02-12 17:54:57 +0200

    upfrontsystems/izinto-angular#230 request config working for queries

--- izinto/services/query.py
+++ izinto/services/query.py
@@ -3,6 +3,7 @@ import json
 from base64 import b64encode
 from datetime import datetime
 from http.client import HTTPSConnection, HTTPConnection
+import requests
 from urllib.parse import urlparse, urlencode
 
 from pyramid.response import Response
@@ -35,7 +36,7 @@ def format_run_query(request, query_string, data, dashboard_id, data_source):
 
     # query with http request parameters
     if data_source.type == 'HTTP':
-        return request_params_query(request.accept_encoding, data_source, query_string)
+        return request_config_query(request.accept_encoding, data_source, query_string)
 
     # query directly over http connection
     if data_source.url.startswith('http'):
@@ -44,38 +45,28 @@ def format_run_query(request, query_string, data, dashboard_id, data_source):
     return database_query(data_source, query_string)
 
 
-def request_params_query(accept_encoding, data_source, statement):
-    """ Query over http connection """
-    query_params = {
-        'q': statement,
-        'db': data_source.database
-    }
-    request_params = json.parse(data_source.request)
-    parse = urlparse(request_params['url'])
-    if parse.scheme == 'https':
-        conn = HTTPSConnection(parse.netloc)
-    else:
-        conn = HTTPConnection(parse.netloc)
+def request_config_query(accept_encoding, data_source, statement):
+    """ Query with request params """
 
-    auth = b64encode(b"%s:%s" % (data_source.username.encode('utf-8'),
-                                 data_source.password.encode('utf-8'))).decode("ascii")
-    headers = {"Content-type": "application/x-www-form-urlencoded",
-               "Accept": "application/json",
-               "Authorization": "Basic %s" % auth}
-    if 'gzip' in accept_encoding:
-        headers["Accept-Encoding"] = 'gzip'
+    request_params = json.loads(data_source.request)
+    # add default method
+    if 'method' not in request_params:
+        request_params['method'] = 'GET'
+    # map auth from dict to tuple
+    if isinstance(request_params['auth'], dict):
+        request_params['auth'] = (
+            request_params['auth']['username'], request_params['auth']['password'])
 
-    request_params['params'] = urlencode(query_params)
-    conn.request(**request_params)
-    remote_response = conn.getresponse()
-    for header in ('Content-Encoding', 'Content-Type'):
-        header_value = remote_response.headers.get('Content-Encoding')
-        if header_value:
-            headers[header] = header_value
-    headers = list(headers.items())
-    response = Response(remote_response.read(), status=remote_response.status, headerlist=headers,
-                        content_type=remote_response.headers.get('Content-Type'))
-    return response
+    # add query to params
+    query_params = request_params.get('params', {})
+    query_params['q'] = statement
+    request_params['params'] = query_params
+
+    remote_response = requests.request(**request_params)
+    # raise any error responses
+    remote_response.raise_for_status()
+    # return data as json
+    return remote_response.json()
 
 
 def http_query(accept_encoding, data_source, statement):
